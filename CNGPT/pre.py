f8b73812cf85d42b576a25216231dd812bbf2332
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-03-16 16:05:48 +0800

    Update pre.py

--- CNGPT/pre.py
+++ CNGPT/pre.py
@@ -18,10 +18,10 @@ Sample = utils.sample#示例
 model_path = '/content/drive/MyDrive/ColabNotebooks/CNGPT/Models/model.bin'
 #训练数据的地址
 path = '/content/drive/MyDrive/ColabNotebooks/CNGPT/datas/train.text'#linux
-#path = 'datas\\train.text'#windows
+
 
 # 分词
-
+#path = 'datas\\train.text'#windows
 f = open(path,encoding='utf-8').read()
 aa = jieba.lcut(f)
 print(aa)
@@ -37,7 +37,7 @@ print(model)
 print("==============================STARTN=================================")
 
 
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load(model_path,map_location='cpu'))
 #当出现 RuntimeError: Attempting to deserialize object on a CUDA device but torch.cuda.is_available() is False. If you are running on a CPU-only machine, please use torch.load with map_location=torch.device('cpu') to map your storages to the CPU.使用下方的方法将其添加到加载模型的语句中
 #map_location=cpu
 
@@ -46,9 +46,9 @@ model.load_state_dict(torch.load(model_path))
 
 # sample from the model (the [None, ...] and [0] are to push/pop a needed dummy batch dimension)
 context = str(input('请给个文本:'))
-
+steps = int(input("输入生成的字数："))
 x = torch.tensor([train_dataset.stoi[s] for s in context], dtype=torch.long)[None,...] # context conditioning
-y = Sample(model, x, steps=100, temperature=1.0, sample=True, top_k=10)[0]
+y = Sample(model, x, steps=steps, temperature=1.0, sample=True, top_k=10)[0]
 print(y)
 print('==============================DONE=================================')
 completion = ''.join([train_dataset.itos[int(i)] for i in y])
