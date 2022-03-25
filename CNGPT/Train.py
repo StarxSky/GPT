b7689a663810c4800417a79eb763f09122b849b8
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-03-25 16:16:51 +0800

    Update Train.py

--- CNGPT/Train.py
+++ CNGPT/Train.py
@@ -3,17 +3,17 @@ import jieba
 import os
 import numpy as np
 
-from Dtasest import MyDataset
-from Layers import Config
-from Layers import utils
-from Layers.Model import GPT_Model
+from Core.Datasets import MyDataset
+from Core import CONFIG
+from Core import Function
+from Core.Model import GPT_Model
 
 
 GPT = GPT_Model#模型
-GPTconfig = Config.GPTConfig#模型配置
-Trainer = Config.Trainer#模型训练器
-Trainerconfig = Config.TrainerConfig#训练配置
-Sample = utils.sample#示例
+GPTconfig = CONFIG.GPTConfig#模型配置
+Trainer = CONFIG.Trainer#模型训练器
+Trainerconfig = CONFIG.TrainerConfig#训练配置
+Sample = Function.sample#示例
 
 
 
@@ -46,11 +46,5 @@ tconf = Trainerconfig(max_epochs=epochs, batch_size=batch_size)
 trainer = Trainer(model, train_dataset, test_dataset=None, config=tconf, Save_Model_path='C:\\Users\\xbj0916\\Desktop\\M')
 trainer.train()
 
-# (... enjoy the show for a while... )
 
 
-
-# sample from the model (the [None, ...] and [0] are to push/pop a needed dummy batch dimension)
-#x = torch.tensor([1, 2, 3], dtype=torch.long)[None, ...] # 语境条件反射
-#y = Sample(model, x, steps=30, temperature=1.0, sample=True, top_k=5)[0]
-#print(y) # 我们的模型用 30 个额外的可能整数填充整数序列
