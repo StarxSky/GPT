e48fc78ad751b8bad026249c04531d730b81058a
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-03-25 16:11:48 +0800

    Creat New GPT-2 model core

---
+++ CNGPT/Core/Datasets.py
@@ -0,0 +1,77 @@
+import torch
+
+from torch.utils import data
+
+
+
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, data, block_size):
+        chars = sorted(list(set(data)))
+        data_size, vocab_size = len(data), len(chars)
+        print('data has %d characters, %d unique.' % (data_size, vocab_size))
+
+        self.stoi = {ch: i for i, ch in enumerate(chars)}
+        self.itos = {i: ch for i, ch in enumerate(chars)}
+        self.block_size = block_size
+        self.vocab_size = vocab_size
+        self.data = data
+
+    def __len__(self):
+        return len(self.data) - self.block_size
+
+    def __getitem__(self, idx):
+        # grab a chunk of (block_size + 1) characters from the data
+        chunk = self.data[idx:idx + self.block_size + 1]
+        # encode every character to an integer
+        dix = [self.stoi[s] for s in chunk]
+        """
+        arrange data and targets so that the first i elements of x
+        will be asked to predict the i-th element of y. Notice that
+        the eventual language model will actually make block_size
+        individual predictions at the same time based on this data,
+        so we are being clever and amortizing the cost of the forward
+        pass of the network. So for example if block_size is 4, then
+        we could e.g. sample a chunk of text "hello", the integers in
+        x will correspond to "hell" and in y will be "ello". This will
+        then actually "multitask" 4 separate examples at the same time
+        in the language model:
+        - given just "h", please predict "e" as next
+        - given "he" please predict "l" next
+        - given "hel" predict "l" next
+        - given "hell" predict "o" next
+
+        In addition, because the DataLoader will create batches of examples,
+        every forward/backward pass during traning will simultaneously train
+        a LOT of predictions, amortizing a lot of computation. In particular,
+        for a batched input of integers X (B, T) where B is batch size and
+        T is block_size and Y (B, T), the network will during training be
+        simultaneously training to make B*T predictions, all at once! Of course,
+        at test time we can paralellize across batch B, but unlike during training
+        we cannot parallelize across the time dimension T - we have to run
+        a forward pass of the network to recover the next single character of the 
+        sequence along each batch dimension, and repeatedly always feed in a next
+        character to get the next one.
+
+        So yes there is a big asymmetry between train/test time of autoregressive
+        models. During training we can go B*T at a time with every forward pass,
+        but during test time we can only go B at a time, T times, with T forward 
+        passes.
+        """
+        x = torch.tensor(dix[:-1], dtype=torch.long)
+        y = torch.tensor(dix[1:], dtype=torch.long)
+        return x, y
+
+
+
+"""
+import jieba
+import os
+
+path_ = os.path.join('datas','a.txt')
+f = open(path_,encoding='utf-8').read()
+aa = jieba.lcut(f)
+aa = ''.join(aa)
+# 构建 GPT 模型
+train_dataset = MyDataset(aa,20)
+"""
---
+++ CNGPT/Core/Function.py
@@ -0,0 +1,68 @@
+import torch
+import numpy as np
+
+from torch.nn import functional as F
+
+
+
+# ==============================================
+# 设置种子
+def set_seed(seed):
+    torch.random.seed(seed)
+    torch.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+# ==============================================
+
+
+# ==============================================
+# 设置 K值
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = -float('Inf')
+    return out
+# ==============================================
+
+
+
+
+
+
+
+
+
+# ==============================================
+
+# 生成实力参数调整，和功能适配
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    """
+    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+    the sequence, feeding the predictions back into the model each time. Clearly the sampling
+    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+    of block_size, unlike an RNN that has an infinite context window.
+    """
+    block_size = model.get_block_size()
+    model.eval()
+    for k in range(steps):
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+        logits, _ = model(x_cond)
+        # pluck the logits at the final step and scale by temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop probabilities to only the top k options
+        if top_k is not None:
+            logits = top_k_logits(logits, top_k)
+        # apply softmax to convert to probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution or take the most likely
+        if sample:
+            ix = torch.multinomial(probs, num_samples=1)
+        else:
+            _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
+
+    return x
+
+# ==============================================
