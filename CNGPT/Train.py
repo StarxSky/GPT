064367e7e944e03a1e9f1402e0482d5038afe2fa
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-04-01 23:04:41 +0800

    Update Train.py

--- CNGPT/Train.py
+++ CNGPT/Train.py
@@ -16,16 +16,37 @@ Trainerconfig = CONFIG.TrainerConfig#训练配置
 Sample = Function.sample#示例
 
 
+pp = str(input("输入您存放训练数据的文件夹目录："))
+################
+# 得到文本文件
+def getFiles(dir, suffix): # 查找根目录，文件后缀 
+    res = []
+    for root, directory, files in os.walk(dir):  # =>当前根,根下目录,目录下的文件
+        for filename in files:
+            name, suf = os.path.splitext(filename) # =>文件名,文件后缀
+            if suf == suffix:
+                res.append(os.path.join(root, filename)) # =>吧一串字符串组合成路径
+    return res
+
+
+
+  
+txts = []
+for file in getFiles("datas", '.txt'):  # =>查找以.txt结尾的文件
+       with open(file, "r",encoding='utf-8') as f: 
+            
+            #打开文件
+            data = f.read()   #读取文件
+            txts.append(data)
+        
+f = ''.join(txts)#转化为非数组类型 
 
 
 
 #######功能实现代码：
-train_name = str(input("\nplease inputs your datas:\n请输入您的要训练的数据文件名:"))
+
 batch_size = int(input("\nplease inputs your batch_size:\n请输入您的要训练的batch_size这将取决于您显存的大小(如果您不确定请输入20):"))
 epochs = int(input("\nEpochs:"))
-# 分词
-path_ = os.path.join('datas',train_name)
-f = open(path_,encoding='utf-8').read()
 aa = jieba.lcut(f)
 print(aa)
 
