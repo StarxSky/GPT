7d430fc675ca185803084cc0c65da8b9e3f28e51
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-04-02 08:02:44 +0800

    Update pre.py

--- CNGPT/pre.py
+++ CNGPT/pre.py
@@ -19,11 +19,35 @@ Sample = Function.sample#示例
 #模型的地址
 model_path = str(input("请输入预训练模型的名称在这之前请您确保下载了模型并且确保模型在Pre_models目录下："))
 pre_model_path = os.path.join('Pre_models',model_path)
-
-#训练数据的地址
-train_name = str(input("\nplease inputs your datas:\n请输入您训练模型的数据:"))
 # CUDA确认
 GPU = bool(input("请确认您是否有CUDA(Yes:True,None:False)："))
+
+
+pp = str(input("输入您存放训练数据的文件夹目录："))
+################
+
+def getFiles(dir, suffix): # 查找根目录，文件后缀 
+    res = []
+    for root, directory, files in os.walk(dir):  # =>当前根,根下目录,目录下的文件
+        for filename in files:
+            name, suf = os.path.splitext(filename) # =>文件名,文件后缀
+            if suf == suffix:
+                res.append(os.path.join(root, filename)) # =>吧一串字符串组合成路径
+    return res
+
+
+
+txts = []
+for file in getFiles("datas", '.txt'):  # =>查找以.py结尾的文件
+       with open(file, "r",encoding='utf-8') as f: 
+            
+            #打开文件
+            data = f.read()   #读取文件
+            txts.append(data)
+        
+f = ''.join(txts)#转化为非数组类型 
+
+
 # 分词
 path_ = os.path.join('datas',train_name)
 f = open(path_,encoding='utf-8').read()
