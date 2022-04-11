3c8c8352e2b3c3ebd5977584b2f90ae640ddd365
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-04-11 08:10:37 +0800

    Update __init__.py

--- CNGPT/Core/__init__.py
+++ CNGPT/Core/__init__.py
@@ -1,3 +1,6 @@
-###
-hello_world = 'This is GPT-2 Model welcome!!'
-print(hello_world)
+import torch
+print("===============================================")
+print("GPTCore Load Done!!")
+print("Device :{}".format(torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
+print("PyTorch version :{}".format(torch.__version__))
+print("===============================================)
