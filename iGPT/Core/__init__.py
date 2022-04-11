0a66678e4f62faa4c0fdc11845e7b2725b10c702
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-04-11 08:12:59 +0800

    Update __init__.py

--- iGPT/Core/__init__.py
+++ iGPT/Core/__init__.py
@@ -1,1 +1,6 @@
-
+import torch
+print("===============================================")
+print("GPTCore Load Done!!")
+print("Device :{}".format(torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
+print("PyTorch version :{}".format(torch.__version__))
+print("==============================================")
