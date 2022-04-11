2cef0b16a1e86a2d5ecc42e1eeca027cdc13c320
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-04-11 08:12:24 +0800

    Update Function.py

--- iGPT/Core/Function.py
+++ iGPT/Core/Function.py
@@ -81,3 +81,26 @@ def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
     return x
 
 # ==============================================
+
+
+
+def Conver_ONNX(model,dummy_input,save_model_name:str):
+    # set the model to inference mode
+    model.eval()
+
+
+    # Export the model
+    torch.onnx.export(model,
+                      # model being run
+                      dummy_input,  # model input (or a tuple for multiple inputs)
+                      save_model_name,  # where to save the model
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=10,  # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['modelInput'],  # the model's input names
+                      output_names=['modelOutput'],  # the model's output names
+                      dynamic_axes={'modelInput': {0: 'batch_size'},  # variable length axes
+                                    'modelOutput': {0: 'batch_size'}})
+    print(" ")
+    print('Model has been converted to ONNX')
+
