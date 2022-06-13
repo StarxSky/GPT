264c85052c106c2fc691cae6f44633e86c649f57
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-06-13 19:20:39 +0800

    Add files via upload

--- BNNGPT/__init__.py
+++ BNNGPT/__init__.py
@@ -1,4 +1,6 @@
-#
-#__init__.py
-#
-import os 
+import torch
+print("===============================================")
+print("GPTCore Load Done!!")
+print("Device :{}".format(torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
+print("PyTorch version :{}".format(torch.__version__))
+print("===============================================")
