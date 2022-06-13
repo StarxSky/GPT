0211a935c193aa5689ec77c6962750615483dfa5
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-06-13 19:21:03 +0800

    Delete BNNGPT directory

--- BNNGPT/README.md
+++
@@ -1,1 +0,0 @@
-# Bayesian Neural Network GPT
--- BNNGPT/__init__.py
+++
@@ -1,6 +0,0 @@
-import torch
-print("===============================================")
-print("GPTCore Load Done!!")
-print("Device :{}".format(torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
-print("PyTorch version :{}".format(torch.__version__))
-print("===============================================")
