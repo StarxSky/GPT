3b785e678a6037832e7c8dfb158eea63090e12b3
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-07-21 15:38:58 +0800

    add NweGELUActivate Function

--- CNGPT/Core/Function.py
+++ CNGPT/Core/Function.py
@@ -1,6 +1,8 @@
+import math
 import torch
 import numpy as np
 
+from torch import Tensor
 from torch.nn import functional as F
 
 
@@ -23,6 +25,19 @@ def top_k_logits(logits, k):
     out[out < v[:, [-1]]] = -float('Inf')
     return out
 # ==============================================
+
+
+
+
+
+class NewGELUActivation(torch.nn.Module):
+
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
+    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
+    """
+    def forward(self, input: Tensor) -> Tensor:
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 
 
